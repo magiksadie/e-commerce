793d17f35ed007d530ccce984bac68d4de2d5a3b
sadie.harty <82483760+magiksadie@users.noreply.github.com>
2022-05-23 20:22:28 -0500

    removed allowNull

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -15,7 +15,6 @@ ProductTag.init(
     },
     product_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'product',
         key: 'id',
@@ -23,7 +22,6 @@ ProductTag.init(
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'tag',
         key: 'id',
